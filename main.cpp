9f2f0e17e6c21db4c99c524b23eccece3d598488
Kacper Bogutczak <250946@edu.p.lodz.pl>
2024-04-11 19:10:58 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -36,9 +36,18 @@ int main() {
                 result = number1 + number2;
                 printf("%d + %d = %d\n", number1, number2, result);
                 break;
+            case 2:
+                enterNumbers(&number1, &number2);
+                result = number1 - number2;
+                printf("%d - %d = %d\n", number1, number2, result);
+                break;
+
+
             default:
                 printf("Choose the right option...\n\n");
         }
+
+
     } while (option != 0);
 
     return 0;
